8066c33d6a52bdc7fe9b744cc8234e213cf71f1b
zhexuany <zhexuany@gmail.com>
2017-03-10 17:06:56 +0800

    added readme file

--- src/main/java/edu/global/sgs/api/controller/HomeController.java
+++ src/main/java/edu/global/sgs/api/controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
     @RequestMapping(value = "/")
     public String index() {
         return "index";
